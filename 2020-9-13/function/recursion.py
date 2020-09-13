20748315c34eb16bb427afa8d5b0a74b56db8145
rage-vampire <rage_vampire0626@163.com>
2020-09-13 17:06:41 +0800

    新增for、函数章节练习

---
+++ 2020-9-13/function/recursion.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : recursion.py
+# @Author: Lizi
+# @Date  : 2020/9/13
--- 2020-9-13/lizi_practise/zhi_shu.py
+++ 2020-9-13/lizi_practise/zhi_shu.py
@@ -9,7 +9,7 @@ num = int(input("请输入一个数："))
 
 if num>=1:
     for i in range(2,num):    # 查看因子项
-        if num % i == 0:      # 遍历除所有的因子，如果商为0，则不是质数
+        if num % i == 0:      # 遍历除所有的因子，如果余数为0，则不是质数
             print('{}不是质数！'.format(num))
             print(i,'乘以',num//i,'等于',num)
             break             # 不是质数到此结束，不再执行后面的代码
