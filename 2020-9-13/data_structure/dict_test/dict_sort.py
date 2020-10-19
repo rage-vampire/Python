4648fc32d1b9e97041ca309b56c7d85738c317c1
rage-vampire <rage_vampire0626@163.com>
2020-10-19 10:38:11 +0800

    ''

--- 2020-9-13/data_structure/dict_test/dict_sort.py
+++ 2020-9-13/data_structure/dict_test/dict_sort.py
@@ -3,3 +3,22 @@
 # @File  : dict_sort.py
 # @Author: Lizi
 # @Date  : 2020/10/13
+
+'''按照字典的key排序'''
+def sort_key():
+
+    key_dict = {'a': 1, 'b': 2, 'd': 4, 'c': 3}
+    for i in sorted(key_dict):
+        new_dict = (i, key_dict[i])
+        print(new_dict, end='')
+
+sort_key()
+print('')
+
+'''按照value排序'''
+
+def sort_value():
+    key_dict = {'a': 1, 'b': 5, 'c': 4, 'd': 3}
+    print(sorted(key_dict.items(), key=lambda kv: kv[1]))
+
+sort_value()
--- 2020-9-13/data_structure/dict_test/dict_sum.py
+++ 2020-9-13/data_structure/dict_test/dict_sum.py
@@ -3,3 +3,14 @@
 # @File  : dict_sum.py
 # @Author: Lizi
 # @Date  : 2020/10/13
+
+def sum_value(mydict):
+    sum = 0
+    for i in key_dict.values():
+        sum += i
+    return sum
+
+
+key_dict = {'a': 1, 'b': 2, 'd': 4, 'c': 3}
+Sum = sum_value(key_dict)
+print('字典值的和为：{}'.format(Sum))
