976cdbc5ccdf97d360708b228ebab2d92bb3743a
rage-vampire <rage_vampire0626@163.com>
2020-11-02 20:55:10 +0800

    '删除'

--- 2020-9-13/unittest_yll/my_math.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @File  : my_math.py
-# @Author: Lizi
-# @Date  : 2020/11/2
--- 2020-9-13/unittest_yll/unittest_test.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @File  : unittest_test.py
-# @Author: Lizi
-# @Date  : 2020/11/2
