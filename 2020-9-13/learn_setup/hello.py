55ef03520b7d689bb453bb42a19ae9f5b6f5cb42
rage-vampire <rage_vampire0626@163.com>
2020-11-03 19:58:48 +0800

    '添加setuptools工具和性能测试分析工具'

---
+++ 2020-9-13/learn_setup/hello.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : hello.py
+# @Author: Lizi
+# @Date  : 2020/11/3
---
+++ 2020-9-13/learn_setup/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : setup.py
+# @Author: Lizi
+# @Date  : 2020/11/3
+
+from setuptools import setup
+
+setup(name='firstApp',
+      version='0.0.1',
+      author='Lizi',
+      py_modules=['hello'],
+      platform='X64'
+)
+
